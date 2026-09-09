d67965c072f20eae2f7233d0d37d1bbb517025c0
rajendrachougale355-tech <rajendrachougale355@gmail.com>
2026-09-09 21:04:16 +0530

    Update S3 bucket name in backend configuration

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket       = "your-bucket-name"
+    bucket       = "my-enterprise-tfstate-bucket-unique-12345"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true  # Replaces deprecated dynamodb_table in AWS Provider v6+
