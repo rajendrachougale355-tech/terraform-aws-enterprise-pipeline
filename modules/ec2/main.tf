aa0217d2b2a54f7141df8c8045c1146e60ab59e6
rajendra <rajendrachougale355@gmail.com>
2026-09-09 16:15:58 +0000

    Fix terraform formatting across environments and modules

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "Web_VM" {
-ami           = var.ami_id
-instance_type = var.instance_type
-subnet_id     = var.subnet_id
-vpc_security_group_ids = var.security_group_ids
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
 
-tags = {
-  Name        = "${var.environment}-web-vm"
-  Environment = var.environment
-}
+  tags = {
+    Name        = "${var.environment}-web-vm"
+    Environment = var.environment
+  }
 }
--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-    value = aws_instance.Web_VM.id
-    description = "The ID of the created EC2 instance"
-      
+  value       = aws_instance.Web_VM.id
+  description = "The ID of the created EC2 instance"
+
 }
 output "instance_public_ip" {
-    value = aws_instance.Web_VM.public_ip
-    description = "The public IP address of the created EC2 instance"
+  value       = aws_instance.Web_VM.public_ip
+  description = "The public IP address of the created EC2 instance"
 }
